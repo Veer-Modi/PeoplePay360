858fe1490b7a209ff815148ee6394a500a2fc18f
Khushi-Rajput1820070 <lovelyblinks2007@gmail.com>
2026-09-05 14:39:59 +0530

    chore(payroll): export public payroll module API

---
+++ peoplePay/src/modules/payroll/index.ts
@@ -0,0 +1,7 @@
+export * from "./email-service";
+export * from "./errors";
+export * from "./payroll-engine";
+export * from "./payrun-service";
+export * from "./pdf-service";
+export * from "./salary-rules";
+export * from "./types";
